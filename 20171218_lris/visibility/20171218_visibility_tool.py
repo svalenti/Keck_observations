5d41a38a904e8ec3fb7fe3d36e1a6d95e7cce0db
Azalee Bostroem <abostroem@gmail.com>
2017-12-18 19:02:02 -1000

    updating with new targets for cloudy night

--- 20171218_lris/visibility/20171218_visibility_tool.py
+++ 20171218_lris/visibility/20171218_visibility_tool.py
@@ -16,7 +16,6 @@ from matplotlib import pyplot as plt
 
 from astroplan import download_IERS_A
 download_IERS_A()
-
     
 observatory = 'keck'
 dates = ['2017-12-18']
@@ -25,9 +24,12 @@ dates = ['2017-12-18']
 #magnitude of
 sn2016bkv = choose_targets.target('2016bkv', dates[0], ra='10:18:19.31', dec='+41:25:39.3')
 iPTF14hls = choose_targets.target('14hls', dates[0], ra='09:20:34.30', dec='+50:41:46.8')
-sn2016fff = choose_targets.target('2016fff', dates[0], ra='22:36:59.14', dec='-25:13:55.6')
 sn2017fzw = choose_targets.target('sn2017fzw', dates[0], ra='06:21:34.77', dec='-27:12:53.5')
-other_objects = [sn2016bkv, iPTF14hls, sn2016fff, sn2017fzw]
+sn2017fgc = choose_targets.target('sn2017fgc', dates[0], ra= '01:20:14.45',  dec='+03:24:10.1')
+sn2017fbu = choose_targets.target('sn2017fbu', dates[0], ra='02:11:06.98',   dec='+03:50:36.4') 
+sn2015fel = choose_targets.target('sn2015fel', dates[0], ra='03:09:12.76', dec='+27:31:16.95') 
+sn2017hyy = choose_targets.target('sn2017hyy', dates[0], ra='04:33:05.96',  dec='-26:07:40.5')
+other_objects = [sn2016bkv, iPTF14hls, sn2017fzw, sn2017fgc, sn2017fbu, sn2015fel, sn2017hyy]
 
 for date in dates:
     ofile = open('{}_{}_targets.txt'.format(date, observatory), 'w')
@@ -46,15 +48,16 @@ for date in dates:
             label.append(sn.name)
     for sn in other_objects:
         sn.date = Time(date)
-        sn, ax = choose_targets.check_visibility(sn, observatory, ax)
+        sn, ax = choose_targets.check_visibility(sn, observatory, ax, fmt=':')
         if sn.plotted is True:
             label.append(sn.name)
     ofile.close()
     ax = choose_targets.add_airmass_axis(ax)
     ax = choose_targets.add_twilight(ax, observatory, date)
     label.append('12deg twilight')
-    ax.legend(label, bbox_to_anchor=(1.10, 1), fontsize='x-small')
+    leg = ax.legend(label, bbox_to_anchor=(1.10, 1), fontsize='x-small')
+    leg.draggable()
     ax.grid()
     ax.set_title('{} {}'.format(date, observatory))
     plt.savefig('{}_{}_visibility.pdf'.format(date, observatory), orientation='landscape')
-    plt.close()
+    #plt.close()
