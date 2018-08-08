7d3d2f0b10855a155254f635083bf5c3a0edaf2f
Azalee Bostroem <abostroem@gmail.com>
2018-08-08 16:54:03 -0700

    adding code to make airmass/altitude plots

--- 20180811_lris/visibility/20180811_visibility_tool.py
+++ 20180811_lris/visibility/20180811_visibility_tool.py
@@ -14,11 +14,12 @@ import choose_targets
 import supernova_typeII_catalog
 from matplotlib import pyplot as plt
 
-from astroplan import download_IERS_A
-download_IERS_A()
+from astroplan import download_IERS_A, plots
+#download_IERS_A()
     
 observatory = 'keck'
 dates = ['2018-08-11']
+plots.dark_style_sheet['axes.prop_cycler']=choose_targets.combine_cycler
 
 #Add objects not in table (e.g. ones that you don't want to calculate the age and current
 #magnitude of
@@ -30,23 +31,26 @@ sn2013fs = choose_targets.target('2013fg', dates[0], ra='23:19:44.67', dec='+10:
 sn2012ec = choose_targets.target('2012ec', dates[0], ra='02:45:59.88', dec='-07:34:27.01') #IIP
 sn2017ivh = choose_targets.target('2017ivh', dates[0], ra='13:39:36.25', dec='-11:28:55.9')
 sn2017ipa = choose_targets.target('2017ipa', dates[0], ra='06:37:58.15', dec='-15:22:00.6')
-other_objects = [sn2018cow, sn2018dfy, sn2017ivv, sn2016iks, sn2013fs, sn2012ec, sn2017ivh, sn2017ipa]
-
+#other_objects = [sn2018cow, sn2018dfy, sn2017ivv, sn2016iks, sn2013fs, sn2012ec, sn2017ivh, sn2017ipa]
+other_objects = []
+my_cycler = [i for i in choose_targets.combine_cycler]
 for date in dates:
     ofile = open('{}_{}_targets.txt'.format(date, observatory), 'w')
     ofile = choose_targets.write_header(ofile, date, observatory)
     fig = plt.figure(figsize=(11, 8.5))
     ax = fig.add_axes([0.125,0.175,0.6, 0.75])
-    ax.axes.set_prop_cycle(choose_targets.cycler_ls)
+    ax.axes.set_prop_cycle(choose_targets.combine_cycler)
     label = []
     tbdata = supernova_typeII_catalog.get_cat()
+    style_indx = 0
     for snname in tbdata['SN']:
-        sn = choose_targets.target(snname, date)
+        sn = choose_targets.target(snname, date, age_min=0)
         sn = choose_targets.calc_mag_age(sn)
-        sn, ax = choose_targets.check_visibility(sn, observatory, ax)
+        sn, ax = choose_targets.check_visibility(sn, observatory, ax, fmt=my_cycler[style_indx])
         choose_targets.write_output_table(ofile, sn)
         if sn.plotted is True:
             label.append(sn.name)
+            style_indx += 1
     for sn in other_objects:
         sn.date = Time(date)
         sn, ax = choose_targets.check_visibility(sn, observatory, ax, fmt=':')
@@ -58,7 +62,10 @@ for date in dates:
     label.append('12deg twilight')
     leg = ax.legend(label, bbox_to_anchor=(1.10, 1), fontsize='x-small')
     leg.draggable()
-    ax.grid()
+    fig = plt.gcf()
+    ax_list = fig.axes
+    ax_list[0].set_zorder(100)
+    ax_list[0].grid(True)
     ax.set_title('{} {}'.format(date, observatory))
     plt.savefig('{}_{}_visibility.pdf'.format(date, observatory), orientation='landscape')
     #plt.close()
